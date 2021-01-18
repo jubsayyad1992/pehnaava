59afaf9fe2d833d08859a1f8f5ae960de886a85c
jubsayyad1992 <66517337+jubsayyad1992@users.noreply.github.com>
2021-01-18 09:27:27 +0530

    Update the menu item withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage/homePage";
 
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
+const JacketsPage = () => (
+  <div>
+    <h1>JacketsPage</h1>
+  </div>
+);
+const SneakersPage = () => (
+  <div>
+    <h1>SneakersPage</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        <Route path="/jackets" component={JacketsPage} />
+        <Route path="/sneakers" component={SneakersPage} />
+      </Switch>
     </div>
   );
 }
